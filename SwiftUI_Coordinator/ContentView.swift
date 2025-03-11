5055dc075cbc4a290c1885686a3fc45741e38dd5
akbarshah19 <akbarshah19@icloud.com>
2025-03-11 22:31:49 +0500

    Initial Commit

---
+++ SwiftUI_Coordinator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Coordinator
+//
+//  Created by Akbarshah Jumanazarov on 3/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Coordinator/SwiftUI_CoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CoordinatorApp.swift
+//  SwiftUI_Coordinator
+//
+//  Created by Akbarshah Jumanazarov on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
